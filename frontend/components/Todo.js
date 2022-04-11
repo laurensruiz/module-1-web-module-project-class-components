c932b7865e40c3d2d5e079e7ecf81f728102a0e3
laurensruiz <laurenruiz97@yahoo.com>
2022-04-11 13:51:56 -0400

    clear works, add and submit works and toggle state works

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default class Todo extends React.Component {
+  handleCLick = () => {
+    this.props.handleToggle(this.props.todo.id);
+  }
   render() {
     return (
       <div>
-        <li>{this.props.todo.name}{this.props.todo.completed? <span>-completed</span> : <span></span>}</li>
+        <li onClick={this.handleCLick}>{this.props.todo.name}{this.props.todo.completed? <span>-completed</span> : <span></span>}</li>
       </div>
     )
   }
