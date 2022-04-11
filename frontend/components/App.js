8cd7f060d12f74cab83634962fe7f7810938a70e
laurensruiz <laurenruiz97@yahoo.com>
2022-04-11 12:41:32 -0400

    mapped existing todo list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,39 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      todos:[
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          name: 'Study Modules',
+          id: 1528817084359,
+          completed: true
+        }
+      ]
+    }
+  }
   render() {
+    const {todos} = this.state;
     return (
       <div>
-        <h1>Things I ned done!</h1>
+        <h1>Things I need done!</h1>
         <ul>
-          <li>Clean Kitchen</li>
-          <li>Cook Breakfast</li>
-          <li>Study Modules</li>
+          {
+            todos.map(todo => {
+              return <li>{todo.name}{todo.completed? <span>-completed</span> : <span></span>}</li>
+            })
+          }
         </ul>
         <form>
           <input/>
